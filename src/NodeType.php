595f83bdeb2719f92be80e28e57f96df8b6df7d4
Dmitriy <dmitriy.gordinskiy94@gmail.com>
2023-09-20 21:53:36 +0200

    Added Schema Node interface and enum for Schema types

---
+++ src/NodeType.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gordinskiy\JsonSchema;
+
+/**
+ * @link https://json-schema.org/understanding-json-schema/reference/type.html
+ */
+enum NodeType: string
+{
+    case String = 'string';
+    case Number = 'number';
+    case Object = 'object';
+    case Array = 'array';
+    case Boolean = 'boolean';
+    case Null = 'null';
+}
