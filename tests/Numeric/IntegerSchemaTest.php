a6da96c42b7e844b799945d5be8546812ec8f76a
gordinskiy <dmitriy.gordinskiy94@gmail.com>
2023-09-25 00:13:30 +0200

    Add tests of generated schema

---
+++ tests/Numeric/IntegerSchemaTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gordinskiy\Tests\Numeric;
+
+use Gordinskiy\JsonSchema\Numeric\IntegerSchema;
+use Gordinskiy\Tests\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class IntegerSchemaTest extends TestCase
+{
+    #[DataProvider('valid_integer_schema_provider')]
+    public function test_json_schema_format(IntegerSchema $schemaObject, string $expectedResult): void
+    {
+        self::assertSame(
+            expected: $expectedResult,
+            actual: json_encode($schemaObject),
+        );
+    }
+
+    public static function valid_integer_schema_provider(): \Generator
+    {
+        yield 'Schema without constraints' => [
+            'Object' => new IntegerSchema(),
+            'Expected result' => '{"type":"integer"}',
+        ];
+        yield 'Schema with multiple of constraints' => [
+            'Object' => new IntegerSchema(multipleOf: 2),
+            'Expected result' => '{"type":"integer","multipleOf":2}',
+        ];
+        yield 'Schema with minimum constraints' => [
+            'Object' => new IntegerSchema(minimum: 999),
+            'Expected result' => '{"type":"integer","minimum":999}',
+        ];
+        yield 'Schema with exclusive minimum constraints' => [
+            'Object' => new IntegerSchema(exclusiveMinimum: 50),
+            'Expected result' => '{"type":"integer","exclusiveMinimum":50}',
+        ];
+        yield 'Schema with maximum constraints' => [
+            'Object' => new IntegerSchema(maximum: 10),
+            'Expected result' => '{"type":"integer","maximum":10}',
+        ];
+        yield 'Schema with exclusive maximum constraints' => [
+            'Object' => new IntegerSchema(exclusiveMaximum: 12),
+            'Expected result' => '{"type":"integer","exclusiveMaximum":12}',
+        ];
+    }
+}
---
+++ tests/Numeric/NumberSchemaTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gordinskiy\Tests\Numeric;
+
+use Gordinskiy\JsonSchema\Numeric\NumberSchema;
+use Gordinskiy\Tests\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class NumberSchemaTest extends TestCase
+{
+    #[DataProvider('valid_number_schema_provider')]
+    public function test_json_schema_format(NumberSchema $schemaObject, string $expectedResult): void
+    {
+        self::assertSame(
+            expected: $expectedResult,
+            actual: json_encode($schemaObject),
+        );
+    }
+
+    public static function valid_number_schema_provider(): \Generator
+    {
+        yield 'Schema without constraints' => [
+            'Object' => new NumberSchema(),
+            'Expected result' => '{"type":"number"}',
+        ];
+        yield 'Schema with multiple of constraints' => [
+            'Object' => new NumberSchema(multipleOf: 2.5),
+            'Expected result' => '{"type":"number","multipleOf":2.5}',
+        ];
+        yield 'Schema with minimum constraints' => [
+            'Object' => new NumberSchema(minimum: 999.9),
+            'Expected result' => '{"type":"number","minimum":999.9}',
+        ];
+        yield 'Schema with exclusive minimum constraints' => [
+            'Object' => new NumberSchema(exclusiveMinimum: 49.9),
+            'Expected result' => '{"type":"number","exclusiveMinimum":49.9}',
+        ];
+        yield 'Schema with maximum constraints' => [
+            'Object' => new NumberSchema(maximum: 10.6),
+            'Expected result' => '{"type":"number","maximum":10.6}',
+        ];
+        yield 'Schema with exclusive maximum constraints' => [
+            'Object' => new NumberSchema(exclusiveMaximum: 12.1),
+            'Expected result' => '{"type":"number","exclusiveMaximum":12.1}',
+        ];
+    }
+}
