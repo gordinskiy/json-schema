fcabc1d82b5d660f28b67b9b0b48c7bff686aaa8
gordinskiy <dmitriy.gordinskiy94@gmail.com>
2023-10-07 00:00:16 +0200

    Add support of Array schema

---
+++ src/Array/ArraySchema.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gordinskiy\JsonSchema\Array;
+
+use Gordinskiy\JsonSchema\GenericSchema;
+use Gordinskiy\JsonSchema\NodeType;
+use Gordinskiy\JsonSchema\SchemaNodeInterface;
+use Webmozart\Assert\Assert;
+
+/**
+ * @link https://json-schema.org/understanding-json-schema/reference/array
+ *
+ * @template-extends GenericSchema<mixed[]>
+ */
+final readonly class ArraySchema extends GenericSchema
+{
+    /**
+     * @inheritDoc
+     *
+     * @param SchemaNodeInterface[]|null $prefixItems
+     */
+    public function __construct(
+        private SchemaNodeInterface|bool|null $items = null,
+        private ?array $prefixItems = null,
+        private ?SchemaNodeInterface $contains = null,
+        private ?int $minContains = null,
+        private ?int $maxContains = null,
+        private ?int $minItems = null,
+        private ?int $maxItems = null,
+        private ?bool $uniqueItems = null,
+
+        ?string $title = null,
+        ?string $description = null,
+        ?string $comment = null,
+        array $default = null,
+        array $const = null,
+        ?array $examples = null,
+        ?array $enum = null,
+        ?bool $readOnly = null,
+        ?bool $writeOnly = null,
+        ?bool $deprecated = null,
+    ) {
+        if ($minContains !== null) {
+            Assert::natural(
+                value: $minContains,
+                message: 'Array minContains constrain value must be a non-negative number.'
+            );
+        }
+
+        if ($maxContains !== null) {
+            Assert::natural(
+                value: $maxContains,
+                message: 'Array maxContains constrain value must be a non-negative number.'
+            );
+        }
+
+        if ($minContains !== null && $maxContains !== null) {
+            Assert::greaterThanEq(
+                value: $maxContains,
+                limit: $minContains,
+                message: 'Array minContains constrain cant be greater than maxContains constrain.'
+            );
+        }
+
+
+        if ($minItems !== null) {
+            Assert::natural(
+                value: $minItems,
+                message: 'Array minItems constrain value must be a non-negative number.'
+            );
+        }
+
+        if ($maxItems !== null) {
+            Assert::natural(
+                value: $maxItems,
+                message: 'Array maxItems constrain value must be a non-negative number.'
+            );
+        }
+
+        if ($minItems !== null && $maxItems !== null) {
+            Assert::greaterThanEq(
+                value: $maxItems,
+                limit: $minItems,
+                message: 'Array minItems constrain cant be greater than maxItems constrain.'
+            );
+        }
+
+        parent::__construct(
+            title: $title,
+            description: $description,
+            comment: $comment,
+            default: $default,
+            const: $const,
+            examples: $examples,
+            enum: $enum,
+            readOnly: $readOnly,
+            writeOnly: $writeOnly,
+            deprecated: $deprecated,
+        );
+    }
+
+    public function jsonSerialize(): array
+    {
+        return array_filter([
+            'type' => NodeType::Array->value,
+            'items' => $this->items,
+            'prefixItems' => $this->prefixItems,
+            'contains' => $this->contains,
+            'minContains' => $this->minContains,
+            'maxContains' => $this->maxContains,
+            'minItems' => $this->minItems,
+            'maxItems' => $this->maxItems,
+            'uniqueItems' => $this->uniqueItems,
+            ...parent::jsonSerialize(),
+        ], static fn (mixed $value) => $value !== null);
+    }
+}
---
+++ tests/Array/ArraySchemaTest.php
@@ -0,0 +1,168 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gordinskiy\Tests\Array;
+
+use Gordinskiy\JsonSchema\Array\ArraySchema;
+use Gordinskiy\JsonSchema\GenericSchema;
+use Gordinskiy\JsonSchema\Numeric\IntegerSchema;
+use Gordinskiy\JsonSchema\Numeric\NumberSchema;
+use Gordinskiy\JsonSchema\String\StringFormat;
+use Gordinskiy\JsonSchema\String\StringSchema;
+use Gordinskiy\Tests\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Webmozart\Assert\InvalidArgumentException;
+
+final class ArraySchemaTest extends TestCase
+{
+    #[
+        DataProvider('valid_schema_provider'),
+        DataProvider('valid_generic_schema_provider')
+    ]
+    public function test_json_schema_format(ArraySchema $schemaObject, string $expectedResult): void
+    {
+        self::assertSame(
+            expected: $expectedResult,
+            actual: json_encode($schemaObject),
+        );
+    }
+
+    public static function valid_schema_provider(): \Generator
+    {
+        yield 'Array without any constraints' => [
+            'Object' => new ArraySchema(),
+            'Expected result' => '{"type":"array"}',
+        ];
+        yield 'Array with items constraint' => [
+            'Object' => new ArraySchema(
+                items: new StringSchema(format: StringFormat::Email)
+            ),
+            'Expected result' => '{"type":"array","items":{"type":"string","format":"email"}}',
+        ];
+        yield 'Array with prefixItem constraint' => [
+            'Object' => new ArraySchema(
+                prefixItems: [new StringSchema(), new IntegerSchema(), new GenericSchema(enum: ["NW", "NE", "SW", "SE"])]
+            ),
+            'Expected result' => '{"type":"array","prefixItems":[{"type":"string"},{"type":"integer"},{"enum":["NW","NE","SW","SE"]}]}',
+        ];
+        yield 'Array with contains constraint' => [
+            'Object' => new ArraySchema(
+                contains: new NumberSchema(const: 3.14)
+            ),
+            'Expected result' => '{"type":"array","contains":{"type":"number","const":3.14}}',
+        ];
+        yield 'Array with minContains constraint' => [
+            'Object' => new ArraySchema(minContains: 6),
+            'Expected result' => '{"type":"array","minContains":6}',
+        ];
+        yield 'Array with maxContains constraint' => [
+            'Object' => new ArraySchema(maxContains: 3),
+            'Expected result' => '{"type":"array","maxContains":3}',
+        ];
+        yield 'Array with minItems constraint' => [
+            'Object' => new ArraySchema(minItems: 2),
+            'Expected result' => '{"type":"array","minItems":2}',
+        ];
+        yield 'Array with maxItems constraint' => [
+            'Object' => new ArraySchema(maxItems: 9),
+            'Expected result' => '{"type":"array","maxItems":9}',
+        ];
+        yield 'Array with uniqueItems constraint' => [
+            'Object' => new ArraySchema(uniqueItems: true),
+            'Expected result' => '{"type":"array","uniqueItems":true}',
+        ];
+    }
+
+    public static function valid_generic_schema_provider(): \Generator
+    {
+        yield 'Schema without constraints. With title' => [
+            'Object' => new ArraySchema(title: 'Schema title'),
+            'Expected result' => '{"type":"array","title":"Schema title"}',
+        ];
+        yield 'Schema without constraints. With description' => [
+            'Object' => new ArraySchema(description: 'Schema without constraints'),
+            'Expected result' => '{"type":"array","description":"Schema without constraints"}',
+        ];
+        yield 'Schema without constraints. With comment' => [
+            'Object' => new ArraySchema(comment: 'Comment test'),
+            'Expected result' => '{"type":"array","comment":"Comment test"}',
+        ];
+        yield 'Schema without constraints. With default value' => [
+            'Object' => new ArraySchema(default: []),
+            'Expected result' => '{"type":"array","default":[]}',
+        ];
+        yield 'Schema without constraints. With const value' => [
+            'Object' => new ArraySchema(const: [5]),
+            'Expected result' => '{"type":"array","const":[5]}',
+        ];
+        yield 'Schema without constraints. With examples' => [
+            'Object' => new ArraySchema(examples: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+            'Expected result' => '{"type":"array","examples":[[1,2,3],[4,5,6],[7,8,9]]}',
+        ];
+        yield 'Schema without constraints. Read only' => [
+            'Object' => new ArraySchema(readOnly: true),
+            'Expected result' => '{"type":"array","readOnly":true}',
+        ];
+        yield 'Schema without constraints. Write only' => [
+            'Object' => new ArraySchema(writeOnly: true),
+            'Expected result' => '{"type":"array","writeOnly":true}',
+        ];
+        yield 'Schema without constraints. Deprecated' => [
+            'Object' => new ArraySchema(deprecated: true),
+            'Expected result' => '{"type":"array","deprecated":true}',
+        ];
+        yield 'Schema with enum constraint.' => [
+            'Object' => new ArraySchema(enum: [[0, 2, 4, 6, 8], [1, 3, 5, 7, 9]]),
+            'Expected result' => '{"type":"array","enum":[[0,2,4,6,8],[1,3,5,7,9]]}',
+        ];
+    }
+
+    public function test_negative_min_contains(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Array minContains constrain value must be a non-negative number.');
+
+        new ArraySchema(minContains: -1);
+    }
+
+    public function test_negative_max_contains(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Array maxContains constrain value must be a non-negative number.');
+
+        new ArraySchema(maxContains: -1);
+    }
+
+    public function test_min_contains_greater_than_max_contains(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Array minContains constrain cant be greater than maxContains constrain.');
+
+        new ArraySchema(minContains: 10, maxContains: 2);
+    }
+
+    public function test_negative_min_items(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Array minItems constrain value must be a non-negative number.');
+
+        new ArraySchema(minItems: -1);
+    }
+
+    public function test_negative_max_items(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Array maxItems constrain value must be a non-negative number.');
+
+        new ArraySchema(maxItems: -1);
+    }
+
+    public function test_min_items_greater_than_max_items(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Array minItems constrain cant be greater than maxItems constrain.');
+
+        new ArraySchema(minItems: 10, maxItems: 2);
+    }
+}
